054a4df74a19546e98fc5806fe1075e419fa739f
David-21110419 <211110419@student.mikroskil.ac.id>
2024-11-01 23:23:45 +0700

    menambahkan judul

--- index.php
+++ index.php
@@ -71,6 +71,7 @@ if ($result_all->num_rows > 0) {
     <h3>pastikan untuk memeriksa harga dan menu dengan benar</h3>
     <h4>gini amat dah</h4>
     <h5>siapa suruh masuk mikroskil</h5>
+    <h6>ehehehehehehheheeeehh</h6>
     <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
